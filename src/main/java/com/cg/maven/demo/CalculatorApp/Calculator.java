2587a1354aed060a9f8d064ffdf50c99530bdd55
NA <shivam.na@capgemini.com>
2018-10-03 16:04:49 +0530

    calculator code is added to github

---
+++ src/main/java/com/cg/maven/demo/CalculatorApp/Calculator.java
@@ -0,0 +1,19 @@
+package com.cg.maven.demo.CalculatorApp;
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+	public int substract(int a, int b) {
+		return a - b;
+	}
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+
+	public double divison(int a, int b) {
+		return (double) a / b;
+	}
+}
